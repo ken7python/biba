b37466291a5db16aa372a6f2bf1dbee1a9e2a12d
ken7python <active.ken.315@gmail.com>
2025-04-10 20:17:35 +0900

    敵を動かし、主人公も追加

--- danmaku.cpp
+++ danmaku.cpp
@@ -31,12 +31,30 @@ struct 弾{
 struct てき{
     int x;
     int y;
+    int xs;
+    int ys;
     int 銃口x;
     int 銃口y;
     int 銃口xs;
+    void 動け(){
+        x = x + xs;
+        y = y + ys;
+        if (800 < x) {
+            xs = -xs;
+        }
+        if (x < 0) {
+            xs = -xs;
+        }
+        if(450 < y) {
+            ys = -ys;
+        }
+        if (y < 0) {
+            ys = -ys;
+        }
+    }
     
     void 弾出す(){
-        弾たち.push_back( new 弾{x , y,銃口x * 3,銃口y*8} );
+        弾たち.push_back( new 弾{x , y,銃口x * 3 + xs,銃口y*8 + ys} );
         銃口x = 銃口x + 銃口xs;
         if (銃口x < -20){
             銃口xs = -銃口xs;
@@ -50,13 +68,38 @@ struct てき{
     }
 };
 
+struct 主人公{
+    int x;
+    int y;
+    void 動け() {
+        if (IsKeyDown(KEY_LEFT)) {
+            x = x - 3;
+        }
+        if (IsKeyDown(KEY_RIGHT)) {
+            x = x + 3;
+        }
+        if (IsKeyDown(KEY_UP)) {
+            y = y - 3;
+        }
+        if (IsKeyDown(KEY_DOWN)) {
+            y = y + 3;
+        }
+    }
+    void draw() {
+        DrawCircle(x, y, 12, YELLOW);
+    }
+};
+
 int main(){
     InitWindow(800, 450, "Danmaku++");
     SetTargetFPS(60);
 
-    てき teki0{ 400, 50, 1, 1, -1 };
+    主人公 shujin0 { 400, 400 };
+    てき teki0{ 400, 50, 3, 3, 1, 1, -1 };
     
     while( !WindowShouldClose() ){
+        shujin0.動け();
+        teki0.動け();
         teki0.弾出す();
 
         {
@@ -84,6 +127,8 @@ int main(){
                     i = i + 1;
                 }
             }
+
+            shujin0.draw();
         EndDrawing();
     }
 
