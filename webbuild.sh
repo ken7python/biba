af26426ac60543c105f52de96ad05e8bf90c83b8
ken7python <active.ken.315@gmail.com>
2025-05-09 10:56:04 +0900

    Clean UI: Removed Emscripten logo and console, optimized canvas layout

--- webbuild.sh
+++ webbuild.sh
@@ -1,11 +1,15 @@
 emcc danmaku.cpp  \
     -o index.html \
     ./libraylib_web.a \
-    --preload-file BGM.mp3 --preload-file gameover.mp3  \
+    --preload-file BGM.mp3 --preload-file gameover.mp3 --preload-file background.png  \
     -s TOTAL_MEMORY=671088640 \
     -s STACK_SIZE=125536 \
     -s USE_GLFW=3 \
     -s ASYNCIFY \
     -s ALLOW_MEMORY_GROWTH=1 \
     -DPLATFORM_WEB \
-    -O2
+    -O2 \
+    -Os \
+    --shell-file=shell_minimal.html
+
+python3 -m http.server 8080
